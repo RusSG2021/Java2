b1f4997084bb48d5fcbb8921d1bb65307e36e660
Ruslan <lhihlohj;l>
2022-01-03 13:39:28 +0300

    lesson 5

---
+++ grsJava2/src/lesson5/lesson5Main.java
@@ -0,0 +1,62 @@
+package lesson5;
+
+import java.util.Arrays;
+
+public class lesson5Main {
+    static final int SIZE = 10000000;
+    static final int h = SIZE / 2;
+
+
+    public static void main(String[] args) throws InterruptedException {
+        float[] arr = new float[SIZE];
+        Arrays.fill(arr, 1);
+
+        long a = System.currentTimeMillis();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + (float) i / 5) * Math.cos(0.2f + (float) i / 5) * Math.cos(0.4f + (float) i / 2));
+        }
+
+        long b = System.currentTimeMillis();
+        System.out.println(b - a);
+
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+
+        long c = System.currentTimeMillis();
+
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < a1.length; i++) {
+                    a1[i] = (float) (a1[i] * Math.sin(0.2f + (float) i / 5) * Math.cos(0.2f + (float) i / 5) * Math.cos(0.4f + (float) i / 2));
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < a2.length; i++) {
+                    a2[i] = (float) ((arr[i] + h) * Math.sin(0.2f + (float) i / 5) * Math.cos(0.2f + (float) i / 5) * Math.cos(0.4f + (float) i / 2));
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+
+        System.out.println(System.currentTimeMillis() - c);
+
+
+    }
+}
